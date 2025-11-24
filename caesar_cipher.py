a736a6ee0ed33b6c951b4af939e60082f4fd4ba5
Gerald O. Calderon <geraldcalderonofficial@gmail.com>
2025-11-24 20:53:50 +0800

    update file

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -4,7 +4,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
             'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-def caesar(start_text, shift_amount, cipher_direction):
+def caesar(start_text):
     end_text = ""
     if cipher_direction == "decode":
         shift_amount *= -1
@@ -12,7 +12,6 @@ def caesar(start_text, shift_amount, cipher_direction):
         if char in alphabet:
             position = alphabet.index(char)
             new_position = position + shift_amount
-            end_text += alphabet[new_position]
         else:
             end_text += char
     print(f"The {cipher_direction}d text is {end_text}")
@@ -20,15 +19,13 @@ def caesar(start_text, shift_amount, cipher_direction):
 from art import logo
 print(logo)
 
-
-should_continue = True
 while should_continue:
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
 
     shift = shift % 26
-    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    caesar(start_text=text)
 
     result = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
     if result == "no":
